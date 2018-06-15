b671c56b67002fdeb1e9f331fe2b3ece0b130369
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-15 03:01:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,3 @@
+function getFirstSelector(selector) {return document.querySelector(selector);}
+function nestedTarget() {return document.querySelector('#nested .target');}
+function increaseRankBy(n) {const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li'); for (let i=0; i<lis.length; i++) {lis[i].innerHTML = (parseInt(lis[i].innerHTML)+parseInt(n)).toString()}}
