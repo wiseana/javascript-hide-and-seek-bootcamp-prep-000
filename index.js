96fbec53937390f3198f3112564e76683369709d
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-15 03:23:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+function getFirstSelector(selector) {return document.querySelector(selector);}
+function nestedTarget() {return document.querySelector('#nested .target');}
+function increaseRankBy(n) {const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li'); for (let i=0; i<lis.length; i++) {lis[i].innerHTML = (parseInt(lis[i].innerHTML)+parseInt(n)).toString()}}
+function deepestChild() {const grand = document.getElementById('app').querySelectorAll('div#grand-node div'); {return grand[grand.length-1]}}
